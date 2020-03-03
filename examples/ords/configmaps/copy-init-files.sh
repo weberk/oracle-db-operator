fa372daba7fac40c4ecbceb83a9edbf6d9a304bd
pdoschki <pdoschki@pirin.de.oracle.com>
2020-03-03 15:55:07 +0100

    fix

--- examples/ords/configmaps/copy-init-files.sh
+++
@@ -1,8 +0,0 @@
-if [ -f /opt/oracle/ords/config/ords/credentials ]; then
-    echo "ords was already initialized..."
-else
-    cp credentials /opt/oracle/ords/config/ords
-    cp cdbAdmin.properties /opt/oracle/ords/config/ords
-    cp post-install.sh /opt/oracle/ords/config/ords
-    chown -R 54321:54321 /opt/oracle/ords/config/ords
-fi
